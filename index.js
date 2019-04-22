b53644a99b25619620673cb2854652299168eda8
damoebius <damo@tamina-online.com>
2019-04-22 18:29:13 +0200

    bandwith

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const app = express()
 
 app.use(express.static('www'));
 
-app.get('/api', function (req, res) {
-  res.send('Hello World!')
+app.post('/api/test/upload', function (req, res) {
+  res.send('')
+})
+
+app.get('/api/test/download', function (req, res) {
+    let startDate = new Date();
+    const duration = 30000;
+    let currentDate = new Date();
+    let buff = Buffer.alloc(50, 'a');
+    while(currentDate.getTime() - startDate.getTime() < duration){
+        currentDate = new Date();        
+        res.write(buff,'binary');
+    }
+    res.end(buff,'binary');
 })
 
 app.listen(3000, function () {
--- www/client.js
+++ www/client.js
@@ -1,1 +1,26 @@
 console.log("client");
+
+let oReq = new XMLHttpRequest();
+oReq.open("GET", "/data.jpg?cacheKiller=" + Math.random(), true);
+oReq.responseType = "arraybuffer";
+let dataSize = 0;
+oReq.onload = function (oEvent) {
+    let endDate = new Date();
+    dataSize = oEvent.total  / 1024 / 1000;
+    let duration = (endDate.getTime() - startDate.getTime()) / 1000; 
+    document.getElementById('downloadSpan').innerHTML = dataSize / duration + ' Mo/s';
+  var arrayBuffer = oReq.response;
+    startDate = new Date();
+    sReq.send(arrayBuffer);
+};
+
+let sReq = new XMLHttpRequest();
+sReq.open("POST", "/api/test/upload", true);
+sReq.onload = function (oEvent) {
+    let endDate = new Date();
+    let duration = (endDate.getTime() - startDate.getTime()) / 1000; 
+    document.getElementById('uploadSpan').innerHTML = dataSize / duration + ' Mo/s';
+};
+
+let startDate = new Date();
+oReq.send(null);
